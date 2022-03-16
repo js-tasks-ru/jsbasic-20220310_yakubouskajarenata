efc5c46f3d3f367de5bd608b54868213979946cc
Yakubouski Aliaksandr <yaaliaksandr86@gmail.com>
2022-03-16 20:58:20 +0100

    factorial solution

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,10 @@
 function factorial(n) {
-  // ваш код...
+	for (result = 1; n > 0; n--)
+		result *= n;
+	return result; // ваш код...
 }
+alert(factorial(0));
+alert(factorial(1));
+alert(factorial(3));
+alert(factorial(5));
+
