7858266d8c79b026bbb310575d5e415de4bef662
Yakubouski Aliaksandr <yaaliaksandr86@gmail.com>
2022-03-18 22:19:38 +0100

    zadacha 1.1(factorial) i 1.2(currentName) 1.3(upprCase) i1.4(spam 1.5(npm test vudaet oshibky)

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,12 @@
 function truncate(str, maxlength) {
-  // ваш код...
+	let strLength = str.length;
+	if (strLength < maxlength) {
+		return (str);
+	} // ваш код...
+	else {
+		let newStr = str.slice(0, maxlength - 1) + '...';
+		return (newStr);
+	}
 }
+truncate('Вот, что мне хотелось бы сказать на эту тему:', 20);
+truncate('Всем привет!', 20);
